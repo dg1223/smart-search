f864ce2d6a576a2fcef1fdf17e6f87ac4007e654
Shamir Alavi <dg1223@users.noreply.github.com>
2019-09-17 17:47:01 -0400

    Added close button, restructured the GUI

--- search_gui.ps1
+++ search_gui.ps1
@@ -5,20 +5,30 @@
     Let's the user find files by searching with filename or any keyword that should exist in the contents of the file
 #>
 
+#------------[Initialisation]------------
+
 # Initialize powershell GUI
 Add-Type -AssemblyName System.Windows.Forms
+
 [System.Windows.Forms.Application]::EnableVisualStyles()
+
+
+#---------------[Form]-------------------
 
 #Create a new form
 $main_form = New-Object System.Windows.Forms.Form
-$main_form.Text = "ITAS G: Drive resources"
+
+# Define form properties
+$main_form.Text = "Smart Search"
 $main_form.Width = 800
 $main_form.Height = 600
 $main_form.AutoSize = $true
+$main_form.KeyPreview = $true
+
 
 # Title
 $title = New-Object System.Windows.Forms.Label
-$title.text = "Opening ITAS resources on G drive"
+$title.text = "Find files in your PC using smart search"
 $title.AutoSize = $true
 $title.Location = New-Object System.Drawing.Point(20,20)
 $title.Font = "Microsoft Sans Serif, 13"
@@ -27,8 +37,6 @@ $title.Font = "Microsoft Sans Serif, 13"
 $description = New-Object System.Windows.Forms.Label
 $description.Text = "Search for any document by entering a filename or any keyword that may exist in the document"
 $description.AutoSize = $true
-# $description.Width = 450
-# $description.Height = 50
 $description.Location = New-Object System.Drawing.Point(22,50)
 $description.Font = "Microsoft Sans Serif, 10"
 
@@ -45,41 +53,58 @@ $search.Size = New-Object System.Drawing.Size(260,20)
 $results = New-Object System.Windows.Forms.TextBox
 $results.Multiline = $true
 $results.Location = New-Object System.Drawing.Point(22, 130)
-$results.Size = New-Object System.Drawing.Size(750, 200)
+$results.Size = New-Object System.Drawing.Size(750, 300)
 $results.ScrollBars = "Vertical"
+
 
 ## Add buttons
 
-# Enter
+# Enter button
 $press_enter = New-Object System.Windows.Forms.Button
 $press_enter.Text = "Enter"
 $press_enter.AutoSize = $true
 $press_enter.Location = New-Object System.Drawing.Point(300,95)
 $press_enter.Font = "Microsoft Sans Serif, 10"
 
-# Add close button
+# Close button
 $close_button = New-Object System.Windows.Forms.Button
 $close_button.Text = "Close"
 $close_button.AutoSize = $true
-$close_button.Location = New-Object System.Drawing.Point(370,400)
+$close_button.Location = New-Object System.Drawing.Point(350,500)
 $close_button.Font = "Microsoft Sans Serif, 10"
 
 # Add elements to the form
-$main_form.Controls.AddRange(@($title, $description, $press_enter, $search, $results, $close_button))
+$main_form.Controls.AddRange(@($title, $description, $press_enter, $close_button, $search, $results))
 
 
-function closeForm()
-{
-    $main_form.close()
-}
+#------------[Functions]------------
 
-function enter_button()
+function search()
 {
     $results.Text += "`r`n" + "testing display: " + $search.Text
 }
 
-$press_enter.Add_Click({ enter_button })
-$close_button.Add_Click({ closeForm })
+function closeForm()
+{
+    $main_form.Close()
+}
+
+
+#------------[Logic]------------
+
+$press_enter.Add_Click( {search} )
+$close_button.Add_Click( {closeForm} )
+
+
+if ($form_openclose_event -eq [System.Windows.Forms.DialogResult]::Cancel)
+{
+    Write-Output 'User pressed close to exit the form.'
+    #$x = $search.Text
+    #$x
+}
+
+#------------[Show form]------------
 
 # Display form
-[void]$main_form.ShowDialog()
+$main_form.TopMost = $true
+$form_openclose_event = $main_form.ShowDialog()
